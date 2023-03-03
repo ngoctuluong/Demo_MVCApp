f5cdb69b172e5f09c07f4aee7bf6cfeebd548477
Tu <Tu@LAPTOP-H2GDL5FF>
2023-03-03 10:18:53 +0700

    Add project files.

---
+++ Demo_MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Demo_MVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo_MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            ViewBag.Message = "Sản phẩm truyền từ Controller:";
+            var prod = new Product
+            {
+                ProductId = 1,
+                ProductName = "Sản phẩm demo",
+                Quatity = 50
+            };
+            return View(prod);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo_MVCApp/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo_MVCApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quatity { get; set; }
+    }
+}
